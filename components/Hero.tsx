60f1bf617a3eccfde36d9c94e4786f3bbf332afe
Armando Terrazas Gómez <A01640924@tec.mx>
2023-06-26 18:57:02 -0600

    Fixed apostrophe in prod

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-[100%] lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
-            Armando's Portfolio
+            Armando&apos;s Portfolio
             <span className="sm:block">  Dashboard </span>
           </h1>
 
