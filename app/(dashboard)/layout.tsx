1eeaa3524da2a3462f0e61269f8f8acf2ac653cd
Armando Terrazas Gómez <A01640924@tec.mx>
2023-12-04 18:54:11 -0600

    Add Firebase authentication and redirect users to login page if not authenticated

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,30 +1,29 @@
 import "../globals.css";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
-import { cookies } from 'next/headers'
 import { redirect } from "next/navigation";
+import { auth } from "@/firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Armando's Portfolio Dashboard",
-  description: "Start now to add your personal information and your projects to your portfolio.",
+  description:
+    "Start now to add your personal information and your projects to your portfolio.",
 };
 
-function validateCookie() {
-  const token = cookies().get("auth")
-  if (token) {
-    return;
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    redirect("/login");
   }
-  redirect("/login")
-}
+});
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  validateCookie()
   return (
     <html lang="en">
       <body className={inter.className}>
